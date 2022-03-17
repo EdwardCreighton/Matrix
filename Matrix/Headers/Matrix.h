d9fb88cec9904c683154fc6229c048b64ce7abc0
EdwardCreighton <eduard.chekukha@mail.ru>
2022-03-17 13:55:48 +0300

    Added Matrix class

---
+++ Matrix/Headers/Matrix.h
@@ -0,0 +1,15 @@
+//
+// Created by home on 17.03.2022.
+//
+
+#ifndef MATRIX_MATRIX_H
+#define MATRIX_MATRIX_H
+
+
+class Matrix
+{
+
+};
+
+
+#endif //MATRIX_MATRIX_H
---
+++ Matrix/Source/Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by home on 17.03.2022.
+//
+
+#include "../Headers/Matrix.h"
